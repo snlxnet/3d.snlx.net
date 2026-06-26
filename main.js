7b0014a3cc2a3170a675d45b4526a12a55238256
Alex <alexanderthestudent@yandex.ru>
2026-06-26 14:11:11 +0300

    now it actually looks good
    note that the file (imrk.glb) was removed due to github being all unhappy about large files

--- main.js
+++ main.js
@@ -3,31 +3,45 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(54, window.innerWidth / window.innerHeight, 1, 10000)
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-scene.background = new THREE.Color(0x181825)
+scene.background = new THREE.Color(0xffffff)
 const controls = new OrbitControls(camera, renderer.domElement)
-camera.position.set(0, -5, 5)
-camera.rotation.x = deg(45)
+camera.position.set(1047, 684, -442)
+camera.rotation.set(-3, 0, 2)
+const initZ = camera.rotation.z
 controls.update()
 
 const loader = new GLTFLoader()
-loader.load('cube.glb', (gltf) => {
-  gltf.scene.children[0].material = new THREE.MeshBasicMaterial({color: 0xffffff})
+let objects = []
+loader.load('imrk.glb', (gltf) => {
+  objects = gltf.scene.children
   console.log('loaded', gltf.scene)
   scene.add(gltf.scene)
 }, undefined, console.error)
 
 function animate(time) {
-  // cube.rotation.z = time / 1000
-  // console.log({p: camera.position, r: camera.rotation})
   renderer.render(scene, camera)
 }
 renderer.setAnimationLoop(animate)
 
-function deg(rad) {
-  return rad*Math.PI/180
+document.addEventListener("keypress", (event) => {
+  if (event.key !== " ") {
+    return
+  }
+
+  const code = `camera.position.set(${fmt(camera.position)})\ncamera.rotation.set(${fmt(camera.rotation)})`
+
+  navigator.clipboard.writeText(code)
+  console.log("Coordinates of the camera copied")
+})
+
+function fmt(vector) {
+  return [vector.x, vector.y, vector.z]
+    .map(axis => Math.floor(axis))
+    .join(", ")
 }
